0105115211e33e7970580ff322a6b8aa231f7934
Isabel Serrato <iserrato@olin.edu>
2021-11-27 19:39:37 -0500

    create one axis file

--- src/airhockey_firmware/src/airhockey_firmware_one_axis/airhockey_firmware_one_axis.ino
+++ src/airhockey_firmware/src/airhockey_firmware_one_axis/airhockey_firmware_one_axis.ino
@@ -1,61 +1,51 @@
 #include <ros.h>
-#include <A4988.h>
 #include <FlexyStepper.h>
 
 #include "pinout.h"
 #include "airhockey_firmware.h"
 
 #define MOTOR_STEPS 200
+FlexyStepper x_stepper;
 
 ros::NodeHandle nh;
 
 // configure the pins connected
-int16_t striker_pos = 0;
-
-FlexyStepper x_stepper;
-FlexyStepper y_stepper1;
-FlexyStepper y_stepper2;
+int16_t striker_pos = 0; // x position of striker
+int16_t delta_pos; // change in position, brought in over ROS
 
 ros::Publisher position_feedback_publisher("/arduino/feedback/striker_pos",
                                            &position_feedback_msg);
 ros::Subscriber<std_msgs::Int16> position_command_subscriber(
     "/arduino/command/striker_pos", &position_command_callback);
+ros::Subscriber<std_msgs::Int16> striker_position_subscriber(
+    "/arduino/command/striker_fb", &striker_position_callback);
 
 void position_command_callback(const std_msgs::Int16& position_cmd) {
-    int16_t delta_pos = position_cmd.data - striker_pos;
+    delta_pos = position_cmd.data - striker_pos;
     striker_pos = position_cmd.data;
-    x_stepper.rotate(delta_pos);
+    x_stepper.moveToPositionInMillimeters(delta_pos);
+}
+
+void striker_position_callback(const std_msgs::Int16& striker_cmd) {
+  x_stepper.setCurrentPositionInMillimeters(striker_cmd.data);
 }
 
 void setup() {
     // Set target motor RPM to 1RPM and microstepping to 1 (full step mode)
     x_stepper.connectToPins(X_STEP_PIN, X_DIR_PIN);
-    x_stepper.setStepsPerMillimeter(25 * 1);
+//    x_stepper.setStepsPerMillimeter(25 * 1); //TODO: is this the right number of steps per mm?
     x_stepper.setSpeedInMillimetersPerSecond(10.0);
     x_stepper.setAccelerationInMillimetersPerSecondPerSecond(10.0);
+    x_stepper.setCurrentPositionInMillimeters(0);
 
-    y_stepper1.connectToPins(Y1_STEP_PIN, Y1_DIR_PIN);
-    y_stepper1.setStepsPerMillimeter(25 * 1);
-    y_stepper1.setSpeedInMillimetersPerSecond(10.0);
-    y_stepper1.setAccelerationInMillimetersPerSecondPerSecond(10.0);
-
-    y_stepper2.connectToPins(Y2_STEP_PIN, Y2_DIR_PIN);
-    y_stepper2.setStepsPerMillimeter(25 * 1);
-    y_stepper2.setSpeedInMillimetersPerSecond(10.0);
-    y_stepper2.setAccelerationInMillimetersPerSecondPerSecond(10.0);
-    
     nh.initNode();
     nh.advertise(position_feedback_publisher);
     nh.subscribe(position_command_subscriber);
 }
 
 void loop() {
-  x_stepper.setCurrentPositionInSteps(0);
-
-  
-    // Tell motor to rotate 360 degrees. That's it.
-    position_feedback_msg.data = striker_pos;
-    position_feedback_publisher.publish(&position_feedback_msg);
-    nh.spinOnce();
-    delay(100);
+  position_feedback_msg.data = striker_pos;
+  position_feedback_publisher.publish(&position_feedback_msg);
+  nh.spinOnce();
+  delay(100);
 }
---
+++ src/airhockey_firmware/src/airhockey_firmware_one_axis/pinout.h
@@ -0,0 +1,7 @@
+#define X_STEP_PIN 8
+#define X_DIR_PIN 9
+#define STEPPER_MS1_PIN 10
+#define STEPPER_MS2_PIN 11
+#define STEPPER_MS3_PIN 12
+
+#define LED_PIN 13
