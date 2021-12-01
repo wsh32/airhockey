0eecf6f6bb31405affd6b2a21a9c1b9ea1104b56
Isabel Serrato <iserrato@olin.edu>
2021-11-30 22:19:43 -0500

    add pinouts and begin defining states

--- src/airhockey_firmware/src/airhockey_state_machine/airhockey_state_machine.ino
+++ src/airhockey_firmware/src/airhockey_state_machine/airhockey_state_machine.ino
@@ -12,6 +12,8 @@ ros::NodeHandle nh;
 // configure the pins connected
 int16_t striker_pos = 0; // x position of striker
 int16_t delta_pos; // change in position, brought in over ROS
+int mode = 0; // changes state: 0 = stop, 1 = run, 2 = homing
+unsigned long last_msg_time = millis();
 
 ros::Publisher position_feedback_publisher("/arduino/feedback/striker_pos",
                                            &position_feedback_msg);
@@ -21,6 +23,11 @@ ros::Subscriber<std_msgs::Int16> striker_position_subscriber(
     "/arduino/command/striker_fb", &striker_position_callback);
 
 void position_command_callback(const std_msgs::Int16& position_cmd) {
+    mode = 1;
+    last_msg_time = millis();
+    digitalWrite(X_EN, LOW); // not sure this is best place for this call
+    digitalWrite(Y1_EN, LOW);
+    digitalWrite(Y2_EN, LOW);
     delta_pos = position_cmd.data;
 }
 
@@ -29,29 +36,67 @@ void striker_position_callback(const std_msgs::Int16& striker_cmd) {
 }
 
 void setup() {
-    // Set target motor RPM to 1RPM and microstepping to 1 (full step mode)
     x_stepper.connectToPins(X_STEP_PIN, X_DIR_PIN);
     x_stepper.setStepsPerMillimeter(400 / (60 * 2)); // 400 steps per rev, 120 mm per rev
     x_stepper.setSpeedInMillimetersPerSecond(10.0);
     x_stepper.setAccelerationInMillimetersPerSecondPerSecond(10.0);
     x_stepper.setCurrentPositionInMillimeters(0);
 
+    y1_stepper.connectToPins(Y1_STEP_PIN, Y1_DIR_PIN);
+    y1_stepper.setStepsPerMillimeter(400 / (60 * 2)); // 400 steps per rev, 120 mm per rev
+    y1_stepper.setSpeedInMillimetersPerSecond(10.0);
+    y1_stepper.setAccelerationInMillimetersPerSecondPerSecond(10.0);
+    y1_stepper.setCurrentPositionInMillimeters(0);
+
+    y2_stepper.connectToPins(Y2_STEP_PIN, Y2_DIR_PIN);
+    y2_stepper.setStepsPerMillimeter(400 / (60 * 2)); // 400 steps per rev, 120 mm per rev
+    y2_stepper.setSpeedInMillimetersPerSecond(10.0);
+    y2_stepper.setAccelerationInMillimetersPerSecondPerSecond(10.0);
+    y2_stepper.setCurrentPositionInMillimeters(0);
+
     nh.initNode();
     nh.advertise(position_feedback_publisher);
     nh.subscribe(position_command_subscriber);
 
     // Breakbeam sensors
-    attachInterrupt(digitalPinToInterrupt(2), update_mode, RISING);
-    attachInterrupt(digitalPinToInterrupt(2), update_mode, RISING);
-    // EStop
-    attachInterrupt(digitalPinToInterrupt(2), update_mode, RISING);
+    attachInterrupt(digitalPinToInterrupt(X_FRONT_BB), stop_mode, RISING);
+    attachInterrupt(digitalPinToInterrupt(X_BACK_BB), stop_mode, RISING);
+    attachInterrupt(digitalPinToInterrupt(Y1_FRONT_BB), stop_mode, RISING);
+    attachInterrupt(digitalPinToInterrupt(Y1_BACK_BB), stop_mode, RISING);
+    attachInterrupt(digitalPinToInterrupt(Y2_FRONT_BB), stop_mode, RISING);
+    attachInterrupt(digitalPinToInterrupt(Y2_BACK_BB), stop_mode, RISING);
+    // EStop -- TODO: figure out which pin this is
+//    attachInterrupt(digitalPinToInterrupt(), update_mode, RISING);
 }
 
 
 void loop() {
-  
-  position_feedback_msg.data = x_stepper.getCurrentPositionInMillimeters();
-  position_feedback_publisher.publish(&position_feedback_msg);
-  nh.spinOnce();
-  delay(100);
+  switch(mode){
+    case 0: // stop
+      delay(1000);
+      nh.spinOnce();
+      break;
+    case 1: // run
+      unsigned long elapsed = millis() - last_msg_time;
+      if (elapsed > 60000) {
+        stop_mode(); // over a minute elapses since last message
+      }
+      x_stepper.processMovement();
+      position_feedback_msg.data = x_stepper.getCurrentPositionInMillimeters();
+      position_feedback_publisher.publish(&position_feedback_msg);
+      nh.spinOnce(); // do we need to spin once every time?
+      delay(100);
+      break;
+    case 2: // homing
+      x_stepper.moveToPositionInMillimeters(0); // might not make sense to keep calling this
+      break;
+  }
+    
+}
+
+void stop_mode() {
+  mode = 0;
+  digitalWrite(X_EN, HIGH); // need to think about where to put these calls
+  digitalWrite(Y1_EN, HIGH);
+  digitalWrite(Y2_EN, HIGH);
 }
--- src/airhockey_firmware/src/airhockey_state_machine/pinout.h
+++ src/airhockey_firmware/src/airhockey_state_machine/pinout.h
@@ -1,9 +1,43 @@
 // use motor 0 as X
-#define X_STEP_PIN 
-#define X_DIR_PIN 9
-#define X_MS1 10
-#define X_MS2 11
-#define X_MS3 12
-#define X_EN 
+#define X_STEP_PIN 19
+#define X_DIR_PIN 18
+#define X_MS1 32
+#define X_MS2 21
+#define X_MS3 20
+#define X_EN 30
+// breakbeam switches
+#define X_FRONT_BB 25
+#define X_BACK_BB 23
+
+// use motor 1 as Y1
+#define Y1_STEP_PIN 36
+#define Y1_DIR_PIN 34
+#define Y1_MS1 42
+#define Y1_MS2 40
+#define Y1_MS3 38
+#define Y1_EN 44
+#define Y1_FRONT_BB 29
+#define Y1_BACK_BB 27
+
+// use motor 2 as Y2
+#define Y2_STEP_PIN 2
+#define Y2_DIR_PIN 3
+#define Y2_MS1 16
+#define Y2_MS2 15
+#define Y2_MS3 14
+#define Y2_EN 17
+#define Y2_FRONT_BB 33
+#define Y2_BACK_BB 31
+
+// use motor 3 as spare motor pins, for reference
+#define SPARE_STEP_PIN 8
+#define SPARE_DIR_PIN 9
+#define SPARE_MS1 5
+#define SPARE_MS2 6
+#define SPARE_MS3 7
+#define SPARE_EN 4
+#define SPARE_FRONT_BB 37
+#define SPARE_BACK_BB 35
+
 
 #define LED_PIN 13
