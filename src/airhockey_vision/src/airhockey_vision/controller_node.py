11e85a2e7738bba90ed569f7e027fafd2bf31af9
Wesley Soo-Hoo <wesoohoo@gmail.com>
2021-12-12 00:49:31 -0500

    add puck position filter, test with puck matching

--- src/airhockey_vision/src/airhockey_vision/controller_node.py
+++ src/airhockey_vision/src/airhockey_vision/controller_node.py
@@ -29,7 +29,9 @@ class Controller:
 
     def get_striker_position(self, time):
         # TODO: Add the math here
-        return self.x_pos, self.y_pos
+        if self.x_pos is not None and self.y_pos is not None:
+            return (self.x_pos - 2.5) * 25.4, self.y_pos * 25.4
+        return 400, 0
 
 
 class ControllerNode:
@@ -44,20 +46,33 @@ class ControllerNode:
         # TODO: add robot constraints
 
         self.controller = Controller()
+        self.enable = True
+        self.center = False
 
         rospy.Timer(rospy.Duration(0.1), self.publish_state)
         rospy.spin()
 
     def puck_state_callback(self, puck_state_msg):
+        self.enable = abs(puck_state_msg.y_vel) > 0.1
+        self.center = puck_state_msg.y_vel > 0.5
+
         self.controller.update_striker_state(0, puck_state_msg.x_pos, 0, 0, 0)
 
     def publish_state(self, event=None):
         x, y = self.controller.get_striker_position(rospy.get_rostime())
 
+        if x is None or y is None:
+            return
+
         header = Header(stamp=rospy.get_rostime())
         point = Point(x=round(x, 2), y=round(y, 2))
-        self.arduino_command_publisher.publish(PointStamped(header=header,
-                                                            point=point))
+
+        #if self.center:
+        #    point = Point(x=400, y=0)
+
+        if True: #self.enable:
+            self.arduino_command_publisher.publish(
+                PointStamped(header=header, point=point))
 
     def striker_state_callback(self, striker_state_msg):
         return
@@ -68,6 +83,7 @@ class ControllerNode:
         y_vel = striker_state_msg.y_vel
 
         self.controller.update_striker_state(time, x_pos, y_pos, x_vel, y_vel)
+
 
 def main():
     rospy.init_node('controller_node', anonymous=True)
